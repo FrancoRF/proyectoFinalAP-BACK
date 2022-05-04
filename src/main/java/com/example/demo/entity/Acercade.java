7374d0293aaab48209e1bfb6b67481a11be2771e
FrancoRF <francoAP2022@gmail.com>
2022-05-04 16:52:59 -0300

    Subiendo el back

---
+++ src/main/java/com/example/demo/entity/Acercade.java
@@ -0,0 +1,51 @@
+package com.example.demo.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Acercade implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String texto;
+	
+	
+	
+	public Acercade() {
+	}
+
+	public Acercade(int id, String texto) {
+		this.id = id;
+		this.texto = texto;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	
+	
+	
+
+}
