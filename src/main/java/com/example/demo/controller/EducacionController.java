af2490daa9769792131fda2311795b97650f080f
FrancoRF <francoAP2022@gmail.com>
2022-05-20 15:16:47 -0300

    .

--- src/main/java/com/example/demo/controller/EducacionController.java
+++ src/main/java/com/example/demo/controller/EducacionController.java
@@ -7,6 +7,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -37,6 +38,7 @@ public class EducacionController {
 	@Autowired
 	UsuarioService usuarioService;
 
+	@PreAuthorize("hasRole('ADMIN')")
 	@GetMapping("/")
 	public ResponseEntity<List<Educacion>> listEducacion(){
 		List<Educacion> list = educacionService.list();
