6b3301d96d88c3973c07a0b3449ff82c15dd4748
FrancoRF <francoAP2022@gmail.com>
2022-05-21 13:13:40 -0300

    .

--- src/main/java/com/example/demo/security/repository/UsuarioRepo.java
+++ src/main/java/com/example/demo/security/repository/UsuarioRepo.java
@@ -3,9 +3,11 @@ package com.example.demo.security.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.demo.security.entity.Usuario;
 
+@Repository
 public interface UsuarioRepo extends JpaRepository<Usuario, Integer>{
 	
 	Optional<Usuario> findByNombreUsuario(String nombreUsuario);
